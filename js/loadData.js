6d48a59d4031fe276d6a1d858f376fb2a55b1900
Adrian Jörg <adi.joerg@hotmail.com>
2020-04-08 00:07:54 +0200

    Can now select year

--- js/loadData.js
+++ js/loadData.js
@@ -1,29 +1,135 @@
-let datasets = [
+import { StateHandler } from "./StateHandler.js";
+
+let datasetsInformation = [
     {"src":"../data/co2_emissions_tonnes_per_person.csv", "title":"CO2 Emissions in tonnes per person", "units":" tonnes per capita", "id": "co2-t-per-capita"},
     {"src":"../data/co2_emissionsInKt.csv", "title":"CO2 Emissions in Kilotonnes", "units" : " Kilotonnes", "id": "co2-kt"}
 ]
 
-let idToDataset = new Map();
-datasets.forEach((dataset) => {
-    idToDataset.set(dataset.id, dataset)
-});
+let idToDataPoints = new Map();
+let data = [];
+let idToData = new Map();
 
-function createQueue(callback) {
+function createQueue(nextCallback) {
+    StateHandler.setState('Loading Data', `Loading the map and ${datasetsInformation.length} Datasets`);
     let queue = d3.queue();
     queue.defer(d3.json, "../map.json")
-    datasets.forEach((dataset) => {
+    datasetsInformation.forEach((dataset) => {
         queue.defer(d3.csv, dataset.src)
     });
-    queue.await(callback)
+    queue.await((error, world, ...data) => {prepareData(nextCallback, error, world, ...data);});
 }
 
-function getDatasetById(id) {
-    return idToDataset.get(id)
+function getDataById(id) {
+    return idToData.get(id)
 }
 
-function getDataset() {
-    return datasets
+function getDataPoints(readData) {
+    let id = readData.id;
+    if (idToDataPoints[id]) return idToDataPoints[id];
+    let dataPoints = readData['columns'];
+    let index = dataPoints.indexOf('country');
+    dataPoints.splice(index, 1);
+    idToDataPoints.set(id, dataPoints);
+    return dataPoints;
 }
 
-export {createQueue, getDataset, getDatasetById}
+function calculateMinMaxMean(dataPoint, data) {
+    let min = Infinity;
+    let max = -Infinity;
+    let total = 0;
+    data.forEach((line) => {
+        min = (+line[dataPoint] < min) ? +line[dataPoint] : min;
+        max = (+line[dataPoint] > max) ? +line[dataPoint] : max;
+        total += +line[dataPoint];
+    });
+
+    let mean = total / data.length;
+
+    return {min, max, mean}
+}
+
+function matchCountry(country, data, dataPoint) {
+    let datasetId = data.information.id;
+    let mean = data.information[dataPoint].get('mean');
+    data.forEach(data => {
+        if (country.properties[datasetId] === undefined) country.properties[datasetId] = {};
+        if (isTheSameCountry(data["country"], country.properties.NAME)) {
+            country.properties[datasetId][dataPoint] = {data: data[dataPoint], isFillValue: false};
+            
+        }
+    });
+    if (country.properties[datasetId][dataPoint] === undefined) {
+        country.properties[datasetId][dataPoint] = {data: mean, isFillValue: true};
+    }
+}
+
+function isTheSameCountry(dataName, jsonName) {
+    let map = {
+        "United States": "United States of America",
+        "Bosnia and Herzegovina": "Bosnia and Herz.",
+        "Central African Republic": "Central African Rep.",
+        "Congo, Dem. Rep.": "Dem. Rep. Congo",
+        "Congo, Rep.": "Congo",
+        "Czech Republic": "Czechia",
+        "Dominican Republic": "Dominican Rep.",
+        "Equatorial Guinea": "Eq. Guinea",
+        "Kyrgyz Republic": "Kyrgyzstan",
+        "Lao": "Laos",
+        "Solomon Islands": "Solomon Is.",
+        "Slovak Republic": "Slovakia",
+        "South Sudan": "S. Sudan",
+        "Cote d'Ivoire": "Côte d'Ivoire",
+    };
+
+    if (dataName == jsonName) return true;
+    if (map[dataName] == jsonName) return true;
+    if (dataName == map[jsonName]) return true;
+
+    return false;
+}
+
+async function prepareData(nextCallback, error, world, ...readData) {
+    StateHandler.setState('Preparing Data', 'Preparing the data...');
+
+    if (error) {
+        StateHandler.setState('An Error occured', error);
+        throw error;
+    };
+
+    
+    let countries = world.objects.countries;
+    // For each read file
+    for (let i = 0; i < readData.length; i++) {
+        //Current file
+        let data = readData[i];
+
+        //Current dataset information
+        let dataset = datasetsInformation[i];
+        data.information = dataset;
+
+        idToData.set(data.information.id, data);
+
+        //Get each year there is data for
+        let dataPoints = getDataPoints(data);
+       
+        dataPoints.forEach(dataPoint => {
+            let {min, max, mean} = calculateMinMaxMean(dataPoint, data);
+            data.information[dataPoint] = new Map([["min", min], ["max", max], ["mean", mean]]);
+            countries.geometries.forEach((element) => {
+                matchCountry(element, data, dataPoint);
+            });
+        });
+
+    }
+
+
+    // exclude antarctica
+    world.objects.countries.geometries.splice(
+        world.objects.countries.geometries.findIndex(d => d.properties.ISO_A2 === 'AQ'),
+        1
+    );
+    nextCallback(error, world, ...readData);
+}
+
+export {createQueue, getDataById}
 
--- js/map.js
+++ js/map.js
@@ -1,85 +1,57 @@
 import {
     createQueue,
-    getDataset,
-    getDatasetById
-} from './loadData.js'
+    getDataById
+} from './loadData.js'; 
+import { StateHandler } from './StateHandler.js';
+
+
+let cartogram;
+let world;
+let statePopover = document.getElementById('statePopover');
+
+//Register self as a delegate
+StateHandler.addStateCallback(()=>{
+    console.log(`New State: ${StateHandler.getStateDescription()}`);
+});
+
+StateHandler.addDisplayInformationCallback(() => {
+    d3.select('#dropdown-text').text(getDataById(StateHandler.getCurrentDisplayedDataset()).information.title);
+    d3.select('#dropdown-text-year').text(StateHandler.getCurrentDisplayedDatapoint());
+});
+
 /**
  * Load the map and all data files
  */
-createQueue(makeMap);
-let cartogram;
-let datasets;
-let selectedID;
+createQueue(showMap);
 
 
-function makeMap(error, world, ...datas) {
-    datasets = getDataset();
-    selectedID = datasets[0].id
-    initializeDropDown();
-    if (error) throw error;
+function showMap(error, worldTopo, ...data) {
+    StateHandler.setState('Displaying the map', 'Displaing a map without any data');
+    world = worldTopo;
+    mapWithoutData();
     
-    let countries = world.objects.countries;
-    console.log(countries);
+    initializeDropDown(data);
+}
 
-    for (let i = 0; i < datas.length; i++) {
-        let data = datas[i];
-        let dataset = datasets[i];
-        let min = Infinity;
-        let max = -Infinity;
-        let total = 0;
-        data.forEach((data) => {
-            min = (+data["2013"] < min) ? +data["2013"] : min;
-            max = (+data["2013"] > max) ? +data["2013"] : max;
-            total += +data["2013"];
-        });
-
-        let mean = total / data.length;
-        console.warn(`Dataset ${dataset.id} min: ${min}, mean: ${mean}, max: ${max}`);
-        dataset.mean = mean;
-        dataset.min = min;
-        dataset.max = max;
-
-        countries.geometries.forEach((element) => {
-            data.forEach(data => {
-                if (isTheSameCountry(data["country"], element.properties.NAME)) {
-                    element.properties[dataset.id] = data["2013"];
-                }
-            });
-            if (element.properties[dataset.id] === undefined) {
-                element.properties[dataset.id] = mean;
-            }
-        });
-    }
-
-
-    // exclude antarctica
-    world.objects.countries.geometries.splice(
-        world.objects.countries.geometries.findIndex(d => d.properties.ISO_A2 === 'AQ'),
-        1
-    );
-
+function mapWithoutData() {
     const colorScale = d3.scaleSequential(d3.interpolateTurbo)
-        .domain([0, Math.max(...world.objects.countries.geometries.map(getGDPPerCapita))]);
+    .domain([0, 1]);
 
-    cartogram = Cartogram()
-        .topoJson(world)
-        .topoObjectName('countries')
-        .value(getDataOfCountry)
-        .color(f => colorScale(getDataOfCountry(f)))
-        .units(' per capita')
-        .valFormatter(d3.format(''))
-        .iterations(1)
-        (document.getElementById('world'));
-
-    function getGDPPerCapita({ properties: p }) {
-        return p[selectedID];
-    }
+cartogram = Cartogram()
+    .topoJson(world)
+    .topoObjectName('countries')
+    .value(1)
+    .units('')
+    .valFormatter((n) => '')
+    .label(({properties: p}) => `${p.NAME}`)
+    .iterations(1)
+    (document.getElementById('world'));
 }
 
 
-function initializeDropDown() {
+function initializeDropDown(data) {
+    let datasets = data.map((data)=> {return data.information});
     let dropdownMenu = document.getElementById('dropdown-menu');
-    console.table(datasets);
     dropdownMenu.innerHTML = '';
     datasets.forEach((dataset) => {
         // <a class="dropdown-item" href="#">Action</a>
@@ -94,43 +66,63 @@ function initializeDropDown() {
     })
 }
 
-function isTheSameCountry(dataName, jsonName) {
-    let map = {
-        "United States": "United States of America",
-        "Bosnia and Herzegovina": "Bosnia and Herz.",
-        "Central African Republic": "Central African Rep.",
-        "Congo, Dem. Rep.": "Dem. Rep. Congo",
-        "Congo, Rep.": "Congo",
-        "Czech Republic": "Czechia",
-        "Dominican Republic": "Dominican Rep.",
-        "Equatorial Guinea": "Eq. Guinea",
-        "Kyrgyz Republic": "Kyrgyzstan",
-        "Lao": "Laos",
-        "Solomon Islands": "Solomon Is.",
-        "Slovak Republic": "Slovakia",
-        "South Sudan": "S. Sudan",
-        "Cote d'Ivoire": "Côte d'Ivoire",
-    };
-
-    if (dataName == jsonName) return true;
-    if (map[dataName] == jsonName) return true;
-    if (dataName == map[jsonName]) return true;
-
-    return false;
+function updateYearPicker() {
+    console.log(getDataById(StateHandler.getCurrentDisplayedDataset()).columns);
+    let columns = getDataById(StateHandler.getCurrentDisplayedDataset()).columns;
+    let dropdownMenu = document.getElementById('dropdown-menu-year');
+    dropdownMenu.innerHTML = '';
+    columns.forEach((col) => {
+        // <a class="dropdown-item" href="#">Action</a>
+        let element = document.createElement('a');
+        element.innerText = ''+col;
+        element.href = "#"
+        element.classList.add('dropdown-item');
+        element.onclick = () => {
+            changeDatapointTo(col);
+        };
+        dropdownMenu.appendChild(element);
+    })
 }
 
 function getDataOfCountry({properties: p}) {
-    return +p[selectedID];
+    let selectedID = StateHandler.getCurrentDisplayedDataset();
+    let selectedDataPoint = StateHandler.getCurrentDisplayedDatapoint();
+    return +p[selectedID][selectedDataPoint].data;
 }
 
 function changeDataSetTo(id) {
-    selectedID = id;
-    let selectedDataset = getDatasetById(selectedID);
-    d3.select('dropdown-text').text(selectedDataset.title);
+    let selectedData = getDataById(id);
+    let dataInformation = selectedData.information;
+    if (selectedData.columns.indexOf(StateHandler.getCurrentDisplayedDatapoint()) == -1) {  
+        StateHandler.changeDisplayedDataset({dataset: dataInformation.id, datapoint: selectedData.columns[selectedData.columns.length -1]});
+    }else {
+        StateHandler.changeDisplayedDataset({dataset: dataInformation.id});
+    }
+
+    let currentDatapoint = StateHandler.getCurrentDisplayedDatapoint();
+    let selectedDatapointInformation = dataInformation[currentDatapoint];
     const colorScale = d3.scaleSequential(d3.interpolatePlasma)
-        .domain([selectedDataset.min, selectedDataset.max]);
+    .domain([selectedDatapointInformation.get('min'), selectedDatapointInformation.get('max')]);
+    cartogram.value(getDataOfCountry)
+    .color((f) => colorScale(getDataOfCountry(f)))
+    .units(selectedDatapointInformation.units)
+    .valFormatter(d3.format(''))
+    .iterations(50);
+
+    StateHandler.setState('Displaying the map', `Displaying the map for ${dataInformation.title} in the year ${currentDatapoint}`)
+    
+    updateYearPicker();
+}
+
+function changeDatapointTo(col) {
+    StateHandler.changeDisplayedDataset({datapoint: col});
+    let currentData = getDataById(StateHandler.getCurrentDisplayedDataset());
+    let dataInformation = currentData.information;
+    let currentDatapoint = StateHandler.getCurrentDisplayedDatapoint();
+    let selectedDatapointInformation = dataInformation[currentDatapoint];
+    const colorScale = d3.scaleSequential(d3.interpolatePlasma)
+        .domain([selectedDatapointInformation.get('min'), selectedDatapointInformation.get('max')]);
     cartogram.value(getDataOfCountry)
         .color((f) => colorScale(getDataOfCountry(f)))
-        .units(selectedDataset.units)
-        .iterations(20);
+        .units(selectedDatapointInformation.units);
 }
